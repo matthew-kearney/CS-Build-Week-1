befb056f9adcd0ccd07ea9a3eafce7d1e7f6843e
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-06-25 17:22:51 -0500

    Updates

---
+++ precache-manifest.04e04bcf6ede34f9a9c9624d6417a10a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b48b6cc02969b936d8dab742564b905",
+    "url": "/%3Cgithub-repo-name%3E/index.html"
+  },
+  {
+    "revision": "4249d14a7e29e6d99146",
+    "url": "/%3Cgithub-repo-name%3E/static/css/main.7ebae9ee.chunk.css"
+  },
+  {
+    "revision": "a8a7efbb2ee6f28b4764",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.52e5dff4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.52e5dff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4249d14a7e29e6d99146",
+    "url": "/%3Cgithub-repo-name%3E/static/js/main.878e266f.chunk.js"
+  },
+  {
+    "revision": "65c4311610a282b39943",
+    "url": "/%3Cgithub-repo-name%3E/static/js/runtime-main.183dc9d5.js"
+  }
+]);
